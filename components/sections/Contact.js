eaeda31ae1075f1f825b0083c193529fdb46e321
Khaled <Khaled>
2025-06-19 20:59:15 +0300

    add fix

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -5,7 +5,8 @@ const Contact = () => (
   >
     <div className="max-w-4xl mx-auto text-center">
       <h2 className="text-3xl sm:text-4xl font-bold text-gray-800 dark:text-white mb-6">
-        Let's Work Together
+
+        
       </h2>
 
       <p className="text-xl text-gray-500 dark:text-gray-400 mb-8 max-w-2xl mx-auto">
